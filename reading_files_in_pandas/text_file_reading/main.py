0a7b9db520a6eeb6b2e070ae9cf489b9c9e28357
Gnifnit <>
2025-11-20 15:02:33 +0000

    Text file reading

---
+++ reading_files_in_pandas/text_file_reading/main.py
@@ -0,0 +1,10 @@
+import pandas as pd
+
+file_url = 'https://codefinity-content-media.s3.eu-west-1.amazonaws.com/a43d24b6-df61-4e11-9c90-5b36552b3437/pandas.txt'
+
+# Write your code below
+text_data = ___
+first_row = ___
+
+# Testing the result
+print(first_row)
